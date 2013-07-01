cbcdd6a805d0a51c260a1dd9c93de051e9a4e558
jairov4 <jairov4@users.noreply.github.com>
2013-07-01 06:42:33 +0000

    better support for intrinsics in GNU

--- src/BitUtil_uint32.h
+++ src/BitUtil_uint32.h
@@ -4,6 +4,8 @@
 #include <cstdint>
 #include "BitUtil.h"
 
+#include <intrin.h>
+
 /// Specialized implementation for 32-bit tokens
 template<bool UseAVX256>
 class BitUtil<uint32_t, UseAVX256>
@@ -12,12 +14,8 @@ public:
 	// No se permite usar AVX256 con tokens de 32 bits
 	BOOST_STATIC_ASSERT(!UseAVX256);
 
+#if defined(_MSC_VER)
 	static bool BitScanForward(unsigned long* idx, uint32_t v)
-	{
-		return _BitScanForward(idx, v) != 0;
-	}
-
-	static bool BitScanReverse(unsigned long* idx, uint32_t v)
 	{
 		return _BitScanForward(idx, v) != 0;
 	}
@@ -36,16 +34,52 @@ public:
 	{
 		return _bittest((const long*)vec, bit) != 0;
 	}
-
-	static void ClearAllBits(uint32_t* vec, unsigned tokens)
-	{
-		memset(vec, 0, tokens * sizeof(uint32_t));	
-	}
-
+	
 	static void OrAndClearSecondBit(uint32_t* vec, unsigned b1, unsigned b2) 
 	{
 		auto c2 = _bittestandreset((long*)vec, b2);
 		if(c2) _bittestandset((long*)vec, b1);	
+	}
+		
+#else
+	static bool BitScanForward(unsigned long* idx, uint64_t v)
+	{
+		auto l = __builtin_ffsll(v);
+		if(l == 0) return false;
+		*idx = l-1;
+		return true;
+	}
+	
+	static bool SetBit(uint64_t* vec, unsigned bit)
+	{
+		*vec |= (uint64_t)1 << bit;
+	}
+
+	static bool ClearBit(uint64_t* vec, unsigned bit)
+	{
+		*vec &= ~((uint64_t)1 << bit);
+	}
+
+	static bool TestBit(const uint64_t* vec, unsigned bit)
+	{
+		return ((*vec >> bit) & 1) != 0;
+	}
+
+	static void OrAndClearSecondBit(uint32_t* vec, unsigned b1, unsigned b2) 
+	{	
+		auto c2 = TestBit(vec, b2);
+		if(c2) 
+		{
+			ClearBit(vec, b2);
+			SetBit(vec, b1);
+		}
+	}
+
+#endif
+		
+	static void ClearAllBits(uint32_t* vec, unsigned tokens)
+	{
+		memset(vec, 0, tokens * sizeof(uint32_t));	
 	}
 
 	static void OrVector(uint32_t* o, const uint32_t* v1, const uint32_t* v2, unsigned s)
@@ -53,6 +87,14 @@ public:
 		while(s--)
 		{
 			*o++ = *v1++ | *v2++;
+		}
+	}
+
+	static void AndVector(uint32_t* o, const uint32_t* v1, const uint32_t* v2, unsigned s)
+	{
+		while(s--)
+		{
+			*o++ = *v1++ & *v2++;
 		}
 	}
 
