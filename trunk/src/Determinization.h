ba928222c08dae258cc8ab6ed4190f4b06c4599e
jairov4@gmail.com <jairov4@gmail.com@97d91c89-05bf-8327-0cbb-4e582a64d134>
2013-07-24 18:53:17 +0000

    add detrm

---
+++ trunk/src/Determinization.h
@@ -0,0 +1,16 @@
+// July 2013, Jairo Andres Velasco Romero, jairov(at)javerianacali.edu.co
+#pragma once
+
+#include "Dfa.h"
+#include "Nfa.h"
+
+template<typename TDfa, typename TNfa>
+class Determinization
+{
+public:
+
+private:
+
+public:
+
+};
