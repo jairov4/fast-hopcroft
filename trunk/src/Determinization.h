a8ba57f55598ca5ec2bc32a4343d82a9a05c1820
jairov4@gmail.com <jairov4@gmail.com@97d91c89-05bf-8327-0cbb-4e582a64d134>
2013-07-25 04:08:50 +0000

    test8

--- trunk/src/Determinization.h
+++ trunk/src/Determinization.h
@@ -18,23 +18,65 @@ private:
 public:
 	TDfa Determinize(TNfa nfa)
 	{	
+		assert(nfa.Initial.any());
+
 		using namespace std;
+
+		typedef tuple<TDfaState,TSymbol,TDfaState> TEdge;
 		
 		vector<TSet> new_states;
-		vector<tuple<TState,TSymbol,TState>>
+		vector<tuple<TState,TSymbol,TState>> new_edges;
+		vector<TDfaState> final_states;
 
-		TSet current=nfa.Initial;
-		TSet next(nfa.GetStates());
+		new_states.push_back(nfa.Initial);
 		
-		for(TSymbol c=0; c<nfa.GetAlphabetLength(); c++)
+		TSet next(nfa.GetStates());
+				
+		size_t new_state_index = 0;
+		size_t current_state_index=0;
+		
+		for(auto current_iterator=new_states.begin(); current_iterator != new_states.end(); current_iterator++)
 		{
-			TState qs = (TState)s;
-			next.clear();
-			for(auto s=current.find_first(); s!=current.npos; s=current.find_next(s))
+			for(TSymbol c=0; c<nfa.GetAlphabetLength(); c++)
 			{
-				next |= nfa.GetSuccessors(qs, c);
+				TState qs = (TState)s;
+				next.clear();
+				auto current = *current_iterator;
+				for(auto s=current.find_first(); s!=current.npos; s=current.find_next(s))
+				{
+					next |= nfa.GetSuccessors(qs, c);
+				}
+				auto found = find(new_states.begin(), new_states.end(), next);
+				if(found == new_states.end())
+				{
+					new_state_index = new_states.size();
+					new_states.push_back(next);
+					// detecta si contiene un final, para marcarlo como final
+					auto finalMask = nfa.Final & next;
+					if(finalMask.any())
+					{
+						final_states.push_back(new_state_index);
+					}
+				} else {
+					new_state_index = found - new_states.begin();
+				}
+				new_edges.push_back(TEdge(current_state_index, c, new_state_index));
 			}
-			auto found = find(new_states.begin(), new_states.end(), next);
-		}		
+			current_state_index++;
+		}
+
+		TDfa dfa(nfa.GetAlphabetLength(), new_states.size());
+		dfa.SetInitial(0);
+		for(auto f : final_states)
+		{
+			dfa.SetFinal(f);
+		}
+		for(auto edge : new_edges)
+		{
+			TDfaState qs = get<0>(edge);
+			TSymbol c = get<1>(edge);
+			TDfaState qt = get<1>(edge);
+			dfa.SetTransition(qs, c, qt);
+		}
 	}
 };
