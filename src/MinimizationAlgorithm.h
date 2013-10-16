b167b0feeae0eb091815b29d0d9af51cca9e097f
jairov4 <jairov4@users.noreply.github.com>
2013-10-16 23:33:29 +0000

    minimization algorithm enum file

---
+++ src/MinimizationAlgorithm.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+
+enum class MinimizationAlgorithm 
+{
+	Hopcroft,
+	Brzozowski,
+	Incremental,
+	Hybrid
+};
+
+std::istream& operator>>(std::istream& in, MinimizationAlgorithm& fmt)
+{
+	std::string token;
+	in >> token;
+	if (token == "hopcroft") fmt = MinimizationAlgorithm::Hopcroft;
+	else if (token == "brzozowski") fmt = MinimizationAlgorithm::Brzozowski;
+	else if (token == "incremental") fmt = MinimizationAlgorithm::Incremental;
+	else if (token == "hybrid") fmt = MinimizationAlgorithm::Hybrid;
+	else throw std::invalid_argument("unknown format");
+	return in;
+}
+
+std::ostream& operator<<(std::ostream& on, const MinimizationAlgorithm& fmt)
+{
+	std::string token;
+	if(fmt == MinimizationAlgorithm::Hopcroft) token = "hopcroft";
+	else if(fmt == MinimizationAlgorithm::Brzozowski) token = "brzozowski";
+	else if(fmt == MinimizationAlgorithm::Incremental) token = "incremental";
+	else if(fmt == MinimizationAlgorithm::Hybrid) token = "hybrid";
+	else throw std::invalid_argument("unknown format");    
+	return on << token;
+}
