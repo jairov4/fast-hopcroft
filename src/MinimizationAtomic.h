44de14ac697d9eb54da72d8b97dc8f37a50391be
jairov4 <jairov4@users.noreply.github.com>
2013-12-04 22:55:30 +0000

    atomico

--- src/MinimizationAtomic.h
+++ src/MinimizationAtomic.h
@@ -13,9 +13,10 @@ class MinimizationAtomic
 public:
 	typedef _TFsa TFsa;
 	typedef _TDfa TDfa;
-	typedef TFsa::TSet TSet;
+	typedef typename TFsa::TSet TSet;
 	typedef typename TFsa::TState TState;
 	typedef typename TFsa::TSymbol TSymbol;
+	typedef typename TFsa::TState TAtomicState;
 
 	typedef Determinization<TFsa, TFsa> TDeterminization;
 
@@ -23,13 +24,23 @@ private:
 
 public:
 
-	void Replica(const TFsa& fsa)
+	void InverseReplicaOfInverse(const TFsa& fsa, 		
+		std::vector<std::list<TState>>& QQ, 
+		std::vector<TAtomicState>& invQQ,
+		std::vector<std::tuple<TAtomicState,TSymbol,TAtomicState>>& transitions, 
+		std::vector<TAtomicState>& II, 
+		std::vector<TState>& splits,
+		TAtomicState& blocks
+		)
 	{
 		using namespace std;
 		const TState INVALID_IDX = -1;
 
-		vector<list<TState>> QQ(1);
-		vector<TState> invQQ(fsa.GetStates(), INVALID_IDX);
+		invQQ.resize(fsa.GetStates());
+		QQ.resize(fsa.GetStates() * 2);
+		fill(invQQ.begin(), invQQ.end(), INVALID_IDX);
+
+		II.clear();
 
 		for(auto i=fsa.GetFinals().GetIterator(); !i.IsEnd(); i.MoveNext())
 		{
@@ -38,49 +49,126 @@ public:
 			invQQ[q] = 0;
 		}
 
-		// esta garantizado que el atomico es de menos o igual numero de estados?
-		vector<tuple<TState,list<TState>>> splitters(fsa.GetStates());
+		// empieza al menos con el bloque que contiene los finales
+		blocks = 1;
+
+		// esta garantizado que el atomico tiene cuando mucho el doble de estados
+		TSet already(fsa.GetStates() * 2);		
+
+		// los estados de la delta directa
 		TSet delta(fsa.GetStates());
-		for(TState currentPartition = 0; currentPartition < QQ.size(); currentPartition++)
+
+		for(TAtomicState currentBlock = 0; currentBlock < blocks; currentBlock++)
 		{
-			auto P = QQ[currentPartition];
-			
+			auto& P = QQ[currentBlock];
+			// si de un split restan cero es porque es un bloque repetido
+			if(splits[currentBlock] == 0) continue;
+
 			for(auto a=0; a<fsa.GetAlphabetLength(); a++)
 			{
 				// calcular delta(P,a)
-				delta.Clear();				
+				delta.Clear();
 				for(auto q=P.begin(); q!=P.end(); q++)
 				{
 					auto d = fsa.GetPredecessors(*q, a);
 					delta.UnionWith(d);
 				}
 
-				// obtiene los splitters de la delta directa				
-				fill(splitters.begin(), splitters.end(), make_tuple(0, list<TState>()));
+				// obtiene los splitters de la delta directa
+				// loose_block usa cero porque nunca el bloque cero es usable (siempre tiene al menos los etados finales)
+				TAtomicState loose_block = 0; 
+				already.Clear();
 				for(auto i=delta.GetIterator(); !i.IsEnd(); i.MoveNext())
 				{
 					auto q = i.GetCurrent();
+										
+					// asegura un bloque para el estado q					
 					auto sp = invQQ[q];
-					if(sp != INVALID_IDX) 
+					TAtomicState dest;
+					if(sp == INVALID_IDX)
 					{
-						auto& splitter = splitters[sp];
-						if(get<0>(splitter) == 0) 
+						if(loose_block == 0) loose_block = blocks++;
+						QQ[loose_block].push_back(q);
+						invQQ[q] = loose_block;
+						dest = loose_block;
+
+						if(fsa.IsInitial(q)) II.push_back(dest);
+					} 
+					else 
+					{
+						// Ya antes habiamos usado el bloque Q
+						if(already.Contains(sp)) continue;
+
+						// Usamos el bloque Q para intenar partir delta
+						const auto& Q = QQ[sp];
+						
+						list<TState> Qp;
+																		
+						// identifica si el bloque efectivamente particionara o 
+						// esta repetido en Q
+						bool isRepeat = true;
+						bool initial = false;
+						for(auto j=Q.begin(); j!=Q.end(); j++)
 						{
-							get<0>(splitter) = QQ[sp].size() - 1;
-						} else {
-							get<0>(splitter)--;
+							if(!delta.TestAndRemove(*j)) 
+							{
+								isRepeat = false;								
+							} else {
+								Qp.push_back(*j);
+								initial ||= fsa.IsInitial(*j);
+							}
 						}
-						get<1>(splitter).push_back(q);
-					}
+
+						already.Add(sp);
+						
+						// si ya estaba repetido seguimos adelante
+						if(isRepeat) continue;
+						
+						dest = blocks++;
+						auto& Qdest = QQ[dest];
+						Qdest.splice(Qdest.begin(), Qp);
+						for(auto i=Qdest.begin(); i!=Qdest.end(); i++)
+							invQQ[*i] = dest;
+
+						if(initial) II.push_back(dest);
+					}					
+					transitions.push_back(make_tuple(dest, a, currentBlock));
 				}
 			}
 		}
 	}
 
-	void Minimize(const TFsa& fsa)
+	TDfa Minimize(const TFsa& fsa)
 	{	
+		using namespace std;
+
 		TDeterminization det;
-		Replica(fsa);
+		vector<list<TState>> QQ;
+		vector<TAtomicState> invQQ;
+		vector<tuple<TAtomicState,TSymbol,TAtomicState>> transitions;
+		vector<TAtomicState> II;
+		vector<TState> splits;
+		TAtomicState blocks;
+
+		InverseReplicaOfInverse(fsa, QQ, invQQ, transitions, II, splits, blocks);
+
+		TFsa fsa_i(fsa.GetAlphabetLength(), QQ.size());
+		fsa_i.SetFinal(0);
+		for(auto i=II.begin(); i!=II.end(); i++)
+		{
+			fsa_i.SetInitial(*i);
+		}
+
+		for(auto i=transitions.begin(); i!=transitions.end(); i++)
+		{
+			fsa_i.SetTransition(get<0>(*i), get<1>(*i), get<2>(*i));
+		}
+
+		TDeterminization::TDfaState dfaNewStates;
+		TDeterminization::TVectorDfaState dfaFinalStates;
+		TDeterminization::TVectorDfaEdge dfaEdges;
+		det.Determinize(fsa_i, &dfaNewStates, dfaFinalStates, dfaEdges);
+		return det.BuildDfa(fsa_i.GetAlphabetLength(), dfaNewStates, dfaFinalStates, dfaEdges);
 	}
 
 };
