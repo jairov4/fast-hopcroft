48e976fc913de8a6b443c6e7b40a6d6283a9f462
jairov4 <jairov4@users.noreply.github.com>
2013-12-10 01:48:54 +0000

    atomico

--- src/MinimizationAtomic.h
+++ src/MinimizationAtomic.h
@@ -68,51 +68,58 @@ public:
 		return str;
 	}
 
-	void InverseReplicaOfInverse(const TDfa& fsa,
+	/* El automata resultante es el inverso de la replica del inverso de fsa
+	QQ Particion de estados resultantes
+	transitions Transiciones resultantes (d'^-1)
+	II Estados iniciales del automata resultante (FF en el paper)
+	*/
+	void ReplicaOfInverse(const TDfa& fsa,
 		TSetOfSets& QQ,		
 		std::vector<TTransition>& transitions,
-		std::vector<TSetOfSetsIterator>& II
+		std::vector<TSetOfSetsIterator>& FF
 		)
 	{
 		using namespace std;
-		const TState INVALID_IDX = -1;
 
-		vector<TAtomicState> invQQ;
+
+		list<TSetOfSetsIterator> LL;
 
 		QQ.clear();
 		QQ.reserve(fsa.GetStates()*2+1);
-
-		vector<TSetOfSetsIterator> LL;
-
-		invQQ.resize(fsa.GetStates());
-		fill(invQQ.begin(), invQQ.end(), INVALID_IDX);
-
 		QQ.insert(fsa.GetFinals());
 		LL.push_back(QQ.begin());
 
-		II.clear();
+		FF.clear();
+		auto intersect_if = fsa.GetFinals();
+		intersect_if.IntersectWith(fsa.GetInitials());
+		if(!intersect_if.IsEmpty())
+		{
+			FF.push_back(QQ.begin());
+		}
 
+		transitions.clear();	
 		// los estados de la delta directa
 		TSet delta(fsa.GetStates());
-		TSetOfSets PP, PmQ;
+		//TSetOfSets PP;
 		ShowConfiguration = true;
 
 		while(!LL.empty())
-		{			
+		{
+			const auto& P = *LL.back();
+
 			if(ShowConfiguration)
 			{
-				cout << "block: " << to_string(*(LL[0])) << endl;
+				cout << "block: " << to_string(P) << endl;
 			}
 
 			for(auto a=0; a<fsa.GetAlphabetLength(); a++)
 			{
-				const auto& P = *LL.begin();
-
+				// paper line: 9
 				// calcular delta(P,a)
 				delta.Clear();
-				for(auto itQ=P->GetIterator(); !itQ.IsEnd(); itQ.MoveNext())
+				for(auto i=P.GetIterator(); !i.IsEnd(); i.MoveNext())
 				{
-					auto q = itQ.GetCurrent();
+					auto q = i.GetCurrent();
 					const auto& d = fsa.GetPredecessors(q, a);
 					delta.UnionWith(d);
 				}
@@ -122,21 +129,27 @@ public:
 					cout << "delta(P, a=" << static_cast<size_t>(a) << ") = " << to_string(delta) << endl;
 				}
 
-				auto rp = TSet::Intersect(delta, *P);
-				auto rn = TSet::Difference(delta, rp);
-
-				if(rp.IsEmpty()) continue;
-
-				auto Sprime = QQ.insert(rp); // QQ U PP
-				transitions.push_back(make_tuple(Sprime.first, a, P));				
-
-				// si insertó es porque es nueva, hay que procesarla luego
-				if(Sprime.second) 
+				// paper line: 10
+				for(auto i=QQ.begin(); i!=QQ.end(); i++)
 				{
-					LL.push_back(Sprime.first);
-				}
+					const auto& S = *i;
+
+					auto rp = TSet::Intersect(delta, S);
+					auto rn = TSet::Difference(delta, rp);
+					if(!rp.IsEmpty() && !rn.IsEmpty()) {
+						auto d = QQ.insert(rp);
+						if(d.second) LL.push_back(d.first);
+						if(!TSet::Intersect(fsa.GetInitials(), rp).IsEmpty()) FF.push_back(d.first);
+						transitions.push_back(make_tuple(d.first, a, *LL.begin()));
+
+						d = QQ.insert(rn);		
+						if(d.second) LL.push_back(d.first);
+						if(!TSet::Intersect(fsa.GetInitials(), rn).IsEmpty()) FF.push_back(d.first);
+						transitions.push_back(make_tuple(d.first, a, *LL.begin()));
+					}
+				}			
 			}
-			LL.erase(LL.begin());
+			LL.pop_front();
 
 			if(ShowConfiguration)
 			{
@@ -159,13 +172,13 @@ public:
 		vector<TTransition> transitions;
 		vector<TSetOfSetsIterator> II;
 
-		InverseReplicaOfInverse(fsa, QQ, transitions, II);
+		ReplicaOfInverse(fsa, QQ, transitions, II);
 
 		TNfa fsa_i(fsa.GetAlphabetLength(), QQ.size());
 		fsa_i.SetFinal(0);
 
 		unordered_map<TSet, TAtomicState, TSet::hash> est;
-		
+
 		for(auto i=QQ.begin(); i!=QQ.end(); i++)
 		{
 			pair<TSet, TAtomicState> o(*i, est.size());
