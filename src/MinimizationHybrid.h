faf059ce8119c1925eeb0af29ca1699347d076a4
jairov4 <jairov4@users.noreply.github.com>
2014-04-30 00:23:33 +0000

    unfinished

--- src/MinimizationHybrid.h
+++ src/MinimizationHybrid.h
@@ -2,6 +2,7 @@
 #pragma once
 
 #include <vector>
+#include <list>
 #include <algorithm>
 #include "Set.h"
 
@@ -14,7 +15,7 @@ public:
 	typedef typename TDfa::TState TState;
 	typedef typename TDfa::TSymbol TSymbol;
 	typedef uint64_t TPairIndex;
-	typedef std::tuple<TState,TState,TSymbol> TSplitter;
+	typedef std::tuple<TState, TState, TSymbol> TSplitter;
 	typedef std::vector<TState> TStateToPartition;
 	typedef std::vector<std::list<TState>> TPartitionSet;
 
@@ -25,7 +26,7 @@ public:
 	public:
 		TStateToPartition state_to_partition;
 		TState new_index;
-		TPartitionSet P;		
+		TPartitionSet P;
 
 		TState GetSize() const { return new_index; }
 
@@ -33,7 +34,7 @@ public:
 		{
 			state_to_partition.resize(states);
 			P.resize(states);
-			for(auto i=P.begin(); i!=P.end(); i++) i->clear();
+			for (auto i = P.begin(); i != P.end(); i++) i->clear();
 			new_index = 0;
 		}
 
@@ -53,23 +54,39 @@ private:
 	TPairIndex GetPairIndex(TState p, TState q) const
 	{
 		assert(p < q);
-		return (q*q-q)/2+p;
+		return (q*q - q) / 2 + p;
 	}
 
-	std::tuple<TState,TState> GetPairFromIndex(TPairIndex index) const
+	std::tuple<TState, TState> GetPairFromIndex(TPairIndex index) const
 	{
-		TState q = static_cast<TState>(sqrt((1+8*index)/4.0f) + 0.5f);
-		TState p = static_cast<TState>(index - (q*q-q)/2);
+		TState q = static_cast<TState>(sqrt((1 + 8 * index) / 4.0f) + 0.5f);
+		TState p = static_cast<TState>(index - (q*q - q) / 2);
 		assert(p < q);
-		assert(index == GetPairIndex(p,q));
+		assert(index == GetPairIndex(p, q));
 
 		return make_tuple(p, q);
 	}
 
-	std::tuple<TState,TState> NormalizedPair(TState p, TState q) const
+	std::tuple<TState, TState> NormalizedPair(TState p, TState q) const
 	{
 		using namespace std;
-		return make_pair(min(p,q), max(p,q));
+		return make_pair(min(p, q), max(p, q));
+	}
+
+	TState Merge(NumericPartition& part, TState p, TState q) const
+	{
+		TState cp = part.Find(p);
+		TState cq = part.Find(q);
+		TState t = cp < cq ? cp : cq;
+		TState s = cp < cq ? cq : cp;
+		auto pt& = part.GetPartition(t);
+		auto ps& = part.GetPartition(s);
+		for (auto i = ps.begin(); i != ps.end(); i++)
+		{
+			part.state_to_partition[*i] = t;
+		}
+		pt.splice(pt.begin(), ps);
+		return t;
 	}
 
 	// Try splits all partition using the splitter indicated by partition index and letter.
@@ -83,13 +100,13 @@ private:
 		BitSet<TState> pred_states(dfa.GetStates());
 		BitSet<TState> block_split(dfa.GetStates());
 		auto& splitter_part = part.GetPartition(splitter_partition_idx);
-		for(auto i=splitter_part.begin(); i!=splitter_part.end(); i++)
+		for (auto i = splitter_part.begin(); i != splitter_part.end(); i++)
 		{
 			TState st = *i;
 			const auto& pred_local = dfa.GetPredecessors(st, splitter_letter);
 			pred_states.UnionWith(pred_local);
 		}
-		if(ShowConfiguration) 
+		if (ShowConfiguration)
 		{
 			cout << "split " << to_string(splitter_part) << "|" << static_cast<size_t>(splitter_letter);
 			cout << " <- " << to_string(pred_states) << endl;
@@ -98,43 +115,43 @@ private:
 		auto old_part_begin = part.P.begin();
 		auto old_part_end = next(old_part_begin, part.new_index);
 		// we ignore new partitions (new ones created inside this loop)
-		for(auto j=pred_states.GetIterator(); !j.IsEnd(); j.MoveNext())
+		for (auto j = pred_states.GetIterator(); !j.IsEnd(); j.MoveNext())
 		{
 			TState st = j.GetCurrent();
 			TState block = part.Find(st);
-			if(block_split.TestAndAdd(block)) continue;
+			if (block_split.TestAndAdd(block)) continue;
 
 			auto& old_part = part.P[block];
 
 			assert(old_part.size() > 0);
-			if(old_part.size() < 2) continue;
+			if (old_part.size() < 2) continue;
 
 			auto& new_part = part.P[part.new_index];
 
-			for(auto j=old_part.begin(); j!=old_part.end(); )
+			for (auto j = old_part.begin(); j != old_part.end();)
 			{
 				auto k = j++;
 				// el bloque al menos contiene una vez un miembro de pred_states
-				if(!pred_states.Contains(*k))
+				if (!pred_states.Contains(*k))
 				{
 					// Q necesita estar por delante de P					
-					if(part.P.begin()+block == cur_part)
+					if (part.P.begin() + block == cur_part)
 					{
-						if(k == i_p)
+						if (k == i_p)
 						{
 							i_p = j;
-							if(i_p != cur_part->end()) i_q = next(i_p, 1);
+							if (i_p != cur_part->end()) i_q = next(i_p, 1);
 						}
-						else if(k == i_q)
+						else if (k == i_q)
 						{
 							i_q = j;
-							if(i_q == cur_part->end()) 
+							if (i_q == cur_part->end())
 							{
 								i_p++;
-								if(i_p == k) i_p = j; // end
+								if (i_p == k) i_p = j; // end
 								else {
 									i_q = next(i_p, 1);
-									if(i_q == k){
+									if (i_q == k){
 										i_q = j; // end
 										i_p = j; // end
 									}
@@ -148,48 +165,89 @@ private:
 					new_part.splice(new_part.end(), old_part, k);
 
 					// p -> q  === !p || q
-					assert( !(i_p == i_q) || (i_p == cur_part->end() && i_q == cur_part->end()) );
-					assert( !(i_p != i_q) || (distance(i_p, i_q) > 0) );
+					assert(!(i_p == i_q) || (i_p == cur_part->end() && i_q == cur_part->end()));
+					assert(!(i_p != i_q) || (distance(i_p, i_q) > 0));
 				}
 			}
-			
+
 			assert(old_part.size() > 0);
-			
+
 			// If we are here, split was done
 			// new partition index must increment
-			if(new_part.size() > 0)	part.new_index++;
+			if (new_part.size() > 0)	part.new_index++;
 		}
 	}
 
-	bool EquivP(TState p, TState q, const TDfa& dfa, const NumericPartition& part, BitSet<TPairIndex>& path, BitSet<TPairIndex>& equiv, std::vector<TSplitter>& splitter_stack)
+	// 0: False
+	// 1: Not known
+	// 2: Pending back reference
+	int AreEquivalent(TState p, TState q,
+		const TDfa& dfa,
+		const NumericPartition& pi, const NumericPartition& ro,
+		BitSet<TPairIndex>& expl,
+		BitSet<TPairIndex>& tocheck, std::vector<TSplitter>& tocheck_stack)
 	{
+		assert(p < q);
 		using namespace std;
-		if(ShowConfiguration) 
+		if (ShowConfiguration)
 		{
 			cout << "Test equiv (" << static_cast<size_t>(p) << ", " << static_cast<size_t>(q) << ")" << endl;
 		}
-		if(part.Find(p) != part.Find(q)) return false;
-		TPairIndex root_pair = GetPairIndex(p,q);
-		if(path.TestAndAdd(root_pair)) return true;		
-		equiv.Add(root_pair);
-		for(TSymbol a=0; a<dfa.GetAlphabetLength(); a++)
-		{
-			TState sp = dfa.GetSuccessor(p,a);
-			TState sq = dfa.GetSuccessor(q,a);
-			if(sp > sq) std::swap(sp, sq);
-			if(sp == sq) continue;
-			TPairIndex pair = GetPairIndex(sp,sq);
-			if(equiv.Contains(pair)) continue;
-			splitter_stack.push_back(make_tuple(sp, sq, a));
-			if(!EquivP(sp,sq, dfa, part, path, equiv, splitter_stack))
-			{
-				return false;
-			}
-			splitter_stack.pop_back();
-			path.Remove(pair);
-		}
 
-		return true;
+		TPairIndex root_pair = GetPairIndex(p, q);
+		expl.Add(root_pair);
+		bool already_seen = false;
+		bool found = false;
+		TSymbol a;
+		for (a = 0; a<dfa.GetAlphabetLength(); a++)
+		{
+			TState sp = dfa.GetSuccessor(p, a);
+			TState sq = dfa.GetSuccessor(q, a);
+			if (sp > sq) std::swap(sp, sq);
+			//if (sp == sq) continue; // condicion sospechosa
+
+			TState pp = pi.Find(sp);
+			TState pq = pi.Find(sq);
+
+			if (pp != pq)
+			{
+				TState minP = pi.GetPartition(pp).size() < pi.GetPartition(pq).size() ? pp : pq;
+				Split(dfa, pi, minP, a);
+				return 0;
+			}
+			else if (expl.Contains(GetPairIndex(sp, sq)))
+			{
+				if (!tocheck.TestAndAdd(GetPairIndex(p, q)))
+				{
+					tocheck_stack.push_back(make_tuple(p, q, a));
+				}
+				already_seen = true;
+			}
+			else if (ro.Find(sp) != ro.Find(sq))
+			{
+				int r = AreEquivalent(sp, sq);
+				if (r == 0)
+				{
+					TState minP = pi.GetPartition(pp).size() < pi.GetPartition(pq).size() ? pp : pq;
+					Split(dfa, pi, minP, a);
+					return 0;
+				}
+				else if (r == 1)
+				{
+					Merge(ro, sp, sq);
+				}
+				else
+				{
+					if (!tocheck.TestAndAdd(GetPairIndex(sp, sq)))
+					{
+						tocheck_stack.push_back(make_tuple(sp, sq, a));
+					}
+					already_seen = true;
+				}
+			}
+		}
+		if (already_seen) return 2;
+		return 1;
 	}
 
 public:
@@ -200,9 +258,9 @@ public:
 		string str;
 		str.append("{");
 		TState cnt = 0;
-		for(auto i : part)
+		for (auto i : part)
 		{
-			if(cnt++ > 0) str += ", ";
+			if (cnt++ > 0) str += ", ";
 			str.append(std::to_string(static_cast<size_t>(i)));
 		}
 		str.append("}");
@@ -214,10 +272,10 @@ public:
 		using namespace std;
 		string str;
 		str.append("{");
-		int cont=0;
-		for(auto i=c.GetIterator(); !i.IsEnd(); i.MoveNext())
+		int cont = 0;
+		for (auto i = c.GetIterator(); !i.IsEnd(); i.MoveNext())
 		{
-			if(cont++ > 0) str.append(", ");
+			if (cont++ > 0) str.append(", ");
 			auto s = static_cast<size_t>(i.GetCurrent());
 			str.append(std::to_string(s));
 		}
@@ -225,7 +283,7 @@ public:
 		return str;
 	}
 
-	
+
 
 	std::string to_string(const NumericPartition& P) const
 	{
@@ -233,9 +291,9 @@ public:
 		string str;
 		TState cnt = 0;
 		str.append("{");
-		for(TState cnt=0; cnt<P.GetSize(); cnt++)
+		for (TState cnt = 0; cnt<P.GetSize(); cnt++)
 		{
-			if(cnt > 0) str.append(", ");
+			if (cnt > 0) str.append(", ");
 			str.append(to_string(P.P[cnt]));
 		}
 		str.append("}");
@@ -251,41 +309,47 @@ public:
 	void Minimize(const TDfa& dfa, NumericPartition& part)
 	{
 		using namespace std;
-		
+
 		TState states = dfa.GetStates();
 		part.Clear(states);
+		NumericPartition ro;
+		ro.Clear(states);
 
 		// Inicializa funcion inversa para obtener la particion a la que pertenece un estado
 		auto it_f = back_inserter(part.P[0]), it_nf = back_inserter(part.P[1]);
-		for(TState st=0; st<dfa.GetStates(); st++)
+		for (TState st = 0; st < states; st++)
 		{
 			auto f = dfa.IsFinal(st);
-			if(f) *it_f++ = st;	else *it_nf++ = st;
+			if (f) *it_f++ = st;	else *it_nf++ = st;
 			part.state_to_partition[st] = f ? 0 : 1;
+
+			ro.state_to_partition[st] = st;
+			ro.P[st].push_back(st);
 		}
 		// partitions count, are all finals?
 		part.new_index = 0;
-		if(!part.P[1].empty()) part.new_index++;
-		if(!part.P[0].empty()) part.new_index++; 
+		if (!part.P[1].empty()) part.new_index++;
+		if (!part.P[0].empty()) part.new_index++;
 		else {
-			swap(part.P[0], part.P[1]);			
+			swap(part.P[0], part.P[1]);
 			replace(part.state_to_partition.begin(), part.state_to_partition.end(), 1, 0);
 		}
-		
-		BitSet<TPairIndex> path((states*states-states)/2);
-		BitSet<TPairIndex> equiv((states*states-states)/2);
+
+		BitSet<TPairIndex> expl((states*states - states) / 2);
+		BitSet<TPairIndex> tocheck((states*states - states) / 2);
+		vector<TSplitter> tocheck_stack;
 		vector<TSplitter> splitter_stack;
 
-		for(auto cur_part=part.P.begin(); cur_part!=next(part.P.begin(), part.new_index); cur_part++)
+		for (auto cur_part = part.P.begin(); cur_part != next(part.P.begin(), part.new_index); cur_part++)
 		{
 			assert(cur_part->size() > 0);
-			if(cur_part->size() < 2) continue;
+			if (cur_part->size() < 2) continue;
 
 			auto i_p = cur_part->begin();
 			auto i_q = next(i_p, 1);
-			while(i_p != cur_part->end() && i_q != cur_part->end())
+			while (i_p != cur_part->end() && i_q != cur_part->end())
 			{
-				if(ShowConfiguration)
+				if (ShowConfiguration)
 				{
 					cout << "pair: " << static_cast<size_t>(*i_p) << ", " << static_cast<size_t>(*i_q) << endl;
 				}
@@ -293,60 +357,64 @@ public:
 				assert(distance(i_p, i_q) > 0);
 
 				TState p, q;
-				tie(p,q) = NormalizedPair(*i_p, *i_q);
+				tie(p, q) = NormalizedPair(*i_p, *i_q);
 
 				assert(p < q);
 				assert(dfa.IsFinal(p) == dfa.IsFinal(q));
-				assert(part.Find(p) == part.Find(q));				
+				assert(part.Find(p) == part.Find(q));
 
-				path.Clear();
-				equiv.Clear();
+				expl.Clear();
+				tocheck.Clear();
+				tocheck_stack.clear();
 				splitter_stack.clear();
 
-				bool isEquiv = EquivP(p, q, dfa, part, path, equiv, splitter_stack);
-				if(ShowConfiguration)
+				bool isEquiv = AreEquivalent(p, q, dfa, part, ro, expl, tocheck, tocheck_stack);
+				if (ShowConfiguration)
 				{
 					cout << (isEquiv ? "YES" : "NO") << endl;
 				}
-				if(isEquiv) 
-				{					
+				if (isEquiv)
+				{
+					// merge equivalent states
+					TState t = Merge(ro, p, q);
+
 					i_q++;
-					if(i_q == cur_part->end()) 
+					if (i_q == cur_part->end())
 					{
 						i_p++;
 						i_q = next(i_p, 1);
 					}
-					continue;
 				}
 
-				while(!splitter_stack.empty())
+				while (!splitter_stack.empty())
 				{
-					TSplitter s = splitter_stack.back();							
+					TSplitter s = splitter_stack.back();
+					splitter_stack.pop_back();
+
 					TState s1 = get<0>(s);
 					TState s2 = get<1>(s);
 					TSymbol a = get<2>(s);
 
-					s1 = part.Find(s1);
-					s2 = part.Find(s2);
+					p1 = part.Find(s1);
+					p2 = part.Find(s2);
 
-					TState min_part;
-					if(part.P[s1].size() < part.P[s2].size())
+					if (p1 == p2)
 					{
-						min_part = s1;
-					} else {
-						min_part = s2;
+						Merge(ro, s1, s2)
 					}
-
-					Split(dfa, part, min_part, a, cur_part, i_p, i_q);
-					splitter_stack.pop_back();
-					if(ShowConfiguration)
+					else
 					{
-						cout << "P=" << to_string(part) << endl;
+						TState min_part = part.P[p1].size() < part.P[p2].size() ? p1 : p2;
+						Split(dfa, part, min_part, a, cur_part, i_p, i_q);
+						if (ShowConfiguration)
+						{
+							cout << "P=" << to_string(part) << endl;
+						}
 					}
 				}
 			}
 		}
-		if(ShowConfiguration)
+		if (ShowConfiguration)
 		{
 			cout << "Finished " << part.GetSize() << " states of " << dfa.GetStates() << endl;
 		}
@@ -356,18 +424,18 @@ public:
 	{
 		TDfa ndfa(dfa.GetAlphabetLength(), partitions.GetSize());
 		int pidx = 0;
-		for(const auto& p : partitions.P)
+		for (const auto& p : partitions.P)
 		{
-			for(TState s : p)
+			for (TState s : p)
 			{
-				for(TSymbol sym=0; sym<dfa.GetAlphabetLength(); sym++)
+				for (TSymbol sym = 0; sym < dfa.GetAlphabetLength(); sym++)
 				{
 					TState tgt = dfa.GetSuccessor(s, sym);
 					TState ptgt = partitions.state_to_partition[tgt];
 					ndfa.SetTransition(pidx, sym, ptgt);
 				}
-				if(dfa.IsInitial(s)) ndfa.SetInitial(pidx);
-				if(dfa.IsFinal(s)) ndfa.SetFinal(pidx);
+				if (dfa.IsInitial(s)) ndfa.SetInitial(pidx);
+				if (dfa.IsFinal(s)) ndfa.SetFinal(pidx);
 			}
 			pidx++;
 		}
